31eec125be7ee3a6e058329a2ce8dcdc35f2e3f3
wookieJ <lukaszjedryczka.biuro@gmail.com>
2018-05-31 20:10:23 +0200

    CORS configuration

--- src/main/java/pl/podwiez/security/CorsConfig.java
+++ src/main/java/pl/podwiez/security/CorsConfig.java
@@ -4,13 +4,13 @@ package pl.podwiez.security;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @EnableWebMvc
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("PUT", "GET", "DELETE", "POST");
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000", "https://localhost:3000", "http://localhost:8080", "https://localhost:8080").allowedMethods("PUT", "GET", "DELETE", "POST");
     }
 }
