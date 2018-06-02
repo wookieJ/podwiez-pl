3a79abe43a0c757fd638d8cfa86fce73fd2930a0
wookieJ <lukaszjedryczka.biuro@gmail.com>
2018-06-02 18:22:02 +0200

    https to allowed origins added

--- src/main/java/pl/podwiez/security/CorsConfig.java
+++ src/main/java/pl/podwiez/security/CorsConfig.java
@@ -11,6 +11,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class CorsConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000", "https://localhost:3000");
     }
 }
