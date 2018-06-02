dd666bbf4c9b29656dcd1b211beca7f6985a3b4d
wookieJ <lukaszjedryczka.biuro@gmail.com>
2018-06-02 18:10:45 +0200

    CORS filtering, csrf disable

--- src/main/java/pl/podwiez/security/SecurityConfig.java
+++ src/main/java/pl/podwiez/security/SecurityConfig.java
@@ -35,6 +35,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 //        http.cors().and().authorizeRequests().anyRequest().authenticated().and().httpBasic().authenticationEntryPoint(authEntryPoint);
         http
+                .cors()
+                .and()
                 .authorizeRequests()
                 .antMatchers("/public/**").permitAll()
                 .anyRequest().authenticated()
