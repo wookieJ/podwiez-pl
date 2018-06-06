8a75aac2a301e879b71c36a5a19e598567344406
wookieJ <lukaszjedryczka.biuro@gmail.com>
2018-06-06 14:18:23 +0200

    Ride endpoint status code 401 fix

--- src/test/java/pl/podwiez/endpoints/RideEndpointsTest.java
+++ src/test/java/pl/podwiez/endpoints/RideEndpointsTest.java
@@ -6,17 +6,20 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.junit.Test;
-
 import java.io.IOException;
-
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
 
 public class RideEndpointsTest {
     @Test
     public void getWithoutAuthorizationThen401Expected() throws IOException {
-        HttpUriRequest request = new HttpGet("http://localhost:8080/rides/");
+        HttpUriRequest request = new HttpGet("https://podwiez-pl.herokuapp.com/rides/");
         HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
         assertThat(httpResponse.getStatusLine().getStatusCode(), equalTo(401));
+    }
+
+    @Test
+    public void test() {
+
     }
 }
